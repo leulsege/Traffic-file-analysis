ea70940e657e3b9f69a49b8d2abfdc988ee31802
Yafet Zerihun <zerihunyafet80@gmail.com>
2024-01-22 19:41:48 +0300

    update training form

--- applications/webapp/src/components/TrainersForm.tsx
+++ applications/webapp/src/components/TrainersForm.tsx
@@ -1,55 +1,78 @@
 import { Link } from "react-router-dom";
-import styles from "./Form.module.css";
+import styles from "./TrainersForm.module.css";
 import { useState } from "react";
 
 function Form() {
   // PRE-FILL FOR DEV PURPOSES
-  const [licenseLevel, setLicenseLevel] = useState("");
-  const [name, setName] = useState("");
   const [licenseNumber, setLicenseNumber] = useState("");
-  const [idNumber, setIdNumber] = useState("");
+  const [trainingType, setTrainingType] = useState("");
+  const [trainingStartDate, setTrainingStartDate] = useState("");
+  const [trainingEndDate, setTrainingEndDate] = useState("");
+  const [trainingPassPoint, setTrainingPassPoint] = useState("");
+  const [trainingResult, setTrainingResult] = useState("");
 
   return (
     <main className={styles.login}>
       <form className={styles.form}>
         <div className={styles.row}>
-          <label htmlFor="text">Name</label>
-          <input
-            type="text"
-            id="name"
-            onChange={(e) => setName(e.target.value)}
-            value={name}
-            required
-          />
-        </div>
-
-        <div className={styles.row}>
-          <label htmlFor="text">License Level</label>
-          <input
-            type="text"
-            id="licenselevel"
-            onChange={(e) => setLicenseLevel(e.target.value)}
-            value={licenseLevel}
-          />
-        </div>
-
-        <div className={styles.row}>
-          <label htmlFor="text">License Number</label>
+          <label htmlFor="text">License Number of Trainer</label>
           <input
             type="text"
             id="licenseNumber"
             onChange={(e) => setLicenseNumber(e.target.value)}
             value={licenseNumber}
+            required
           />
         </div>
 
         <div className={styles.row}>
-          <label htmlFor="text">id Number</label>
+          <label htmlFor="text">Training Type</label>
+          <input
+            type="text"
+            id="trainingType"
+            onChange={(e) => setTrainingType(e.target.value)}
+            value={trainingType}
+            required
+          />
+        </div>
+
+        <div className={styles.row}>
+          <label htmlFor="text">training Start Date</label>
+          <input
+            type="date"
+            id="trainingStartDate"
+            onChange={(e) => setTrainingStartDate(e.target.value)}
+            value={trainingStartDate}
+          />
+        </div>
+
+        <div className={styles.row}>
+          <label htmlFor="text">training EndDate</label>
+          <input
+            type="date"
+            id="training End Date"
+            onChange={(e) => setTrainingEndDate(e.target.value)}
+            value={trainingEndDate}
+          />
+        </div>
+
+        <div className={styles.row}>
+          <label htmlFor="text">training Pass Point</label>
           <input
             type="text"
             id="idNumber"
-            onChange={(e) => setIdNumber(e.target.value)}
-            value={idNumber}
+            onChange={(e) => setTrainingPassPoint(e.target.value)}
+            value={trainingPassPoint}
+          />
+        </div>
+
+        <div className={styles.row}>
+          <label htmlFor="text">Training Result</label>
+          <input
+            type="text"
+            id="trainingResult"
+            onChange={(e) => setTrainingResult(e.target.value)}
+            value={trainingResult}
           />
         </div>
 
