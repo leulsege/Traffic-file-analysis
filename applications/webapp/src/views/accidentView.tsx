f6492187e352113e7a68e244085cf430204e104e
Yafet Zerihun <zerihunyafet80@gmail.com>
2024-01-25 22:02:01 +0300

    add accident edit platform

--- applications/webapp/src/views/accidentView.tsx
+++ applications/webapp/src/views/accidentView.tsx
@@ -1,51 +1,81 @@
 import React from "react";
-import styles from "./DriverProfile.module.css";
+import styles from "./AccidentView.module.css";
 import { useState } from "react";
-import TrainerForm from "../components/TrainerForm";
+import AccidentViewForm from "../components/AccidentViewForm";
+import PhotoUpload from "../components/photoUpload";
 
 const res = {
   data: {
-    trainings: {
-      _id: "659722c69235c356e0646486",
-      trainingType: "Hazardous Materials",
-      trainingStartDate: "2023-06-18T00:00:00.000Z",
-      trainingEndDate: "2023-06-20T00:00:00.000Z",
-      trainingPassPoint: 85,
-      trainingResult: 92,
-      checkUp: "Passed",
+    accident_date: "2022-01-23T12:34:56.789Z",
+    accident_place: "City, Country",
+    damages: "Minor damage to the rear bumper",
+    cause: "Rear-end collision",
+    guilty: "Other driver",
+    damageEstimation: 1500,
+    insuranceSentDate: "2022-01-25T00:00:00.000Z",
+    excessLetterDate: "2022-02-01T00:00:00.000Z",
+    maintenanceProcess: "Body repair and painting",
+    preformDate: "2022-02-10T00:00:00.000Z",
+    paymentDateLetterNumber: "ABC123",
+    paymentRequestLetterDate: "2022-02-15T00:00:00.000Z",
+    givenPoint: 10,
+    reducedPoint: 0,
+    givenDecision: "Compensation granted",
+    photo: null,
+    driver: {
+      _id: "659706741e689e5864529823",
+      name: "Samuel Lee",
+      licenseLevel: "Class B",
+      licenseNumber: "UVW789",
+      licenseExpiredDate: "2024-04-03T00:00:00.000Z",
+      gender: "Male",
+      commencementDate: "2016-12-12T00:00:00.000Z",
+      age: 42,
+      idNumber: "890123456",
+      __v: 0,
+      photo: "driver-659706741e689e5864529823-1704468487049.jpeg",
+      id: "659706741e689e5864529823",
+    },
+    vehicle: {
+      _id: "65971f1eddd2d5c29a602a95",
+      vehicleType: "Van",
+      PlateNumber: 567,
+      MoterNumber: 890,
+      chanciNumber: 123,
+      sideNumber: 141,
+      pmServiceTime: 9000,
+      bmServiceTime: 18000,
+      startingPoint: "City F",
+      destination: "City G",
+      stayingPlace: "City H",
       driver: {
         photo: null,
-        phoneNumber: "0912121212",
-        _id: "659706571e689e586452981d",
-        name: "Charlie Brown",
-        licenseLevel: "Class C",
-        licenseNumber: "GHI123",
-        licenseExpiredDate: "2022-06-18T00:00:00.000Z",
-        gender: "Male",
-        commencementDate: "2018-11-30T00:00:00.000Z",
-        age: 38,
-        idNumber: "678901234",
+        _id: "659706491e689e586452981a",
+        name: "Eva Martinez",
+        licenseLevel: "Class B",
+        licenseNumber: "MNO789",
+        licenseExpiredDate: "2024-11-12T00:00:00.000Z",
+        gender: "Female",
+        commencementDate: "2020-07-05T00:00:00.000Z",
+        age: 29,
+        idNumber: "234567890",
         __v: 0,
-        id: "659706571e689e586452981d",
+        id: "659706491e689e586452981a",
       },
-      __v: 0,
     },
   },
 };
-const driverinf = res.data.trainings.driver;
-console.log(driverinf, "from training inf");
 
-function TrainerProfile() {
+function AccidentView() {
   return (
     <>
       <main className={styles.container}>
         <div className={styles.imgholder}>
-          <img src="driver.jpg" className={styles.driverImg} />
-          <p className={styles.name}>{driverinf.name}</p>
-          <p className={styles.phoneNumber}>{driverinf.phoneNumber}</p>
+          <img src="accident.png" className={styles.accidentImg} />
+          <PhotoUpload />
         </div>
         <div className={styles.profileSettings}>
-          <TrainerForm trainings={res.data.trainings} />
+          <AccidentViewForm accidentData={res.data} />
         </div>
         <div>other display</div>
       </main>
@@ -53,4 +83,4 @@ function TrainerProfile() {
   );
 }
 
-export default TrainerProfile;
+export default AccidentView;
